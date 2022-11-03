d9b0db777fc3c8b93718bc1675c1088248f8a0b5
mringeling17 <matias.ringeling@mail.udp.cl>
2022-11-03 16:37:11 -0300

    sys argv

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from time import time
+import sys
 def main():
     count = 0
-    while(True):
-        print("Hello World!")
-        count += 1
-        if count == 10:
-            break
+    inputs = sys.argv[1:]
+    for i in inputs:
+        print(i)
 
 if __name__ == "__main__":
     main()
